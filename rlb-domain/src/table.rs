c19a40574baa0d52e57065d05b7f04de7e50e2de
Mekurushi <Mekurushi@gmail.com>
2026-06-27 00:22:31 +0200

    refactored table resolve

--- rlb-domain/src/table.rs
+++ rlb-domain/src/table.rs
@@ -6,98 +6,84 @@ use crate::table_view::TableView;
 use rlb_error::Result;
 
 #[derive(Debug, Clone)]
-pub enum TableKind {
-    BackFromAttractionScriptList(TableView<BackFromAttractionScriptList>),
-    EnterZoneScriptList(TableView<EnterZoneScriptList>),
-    FsbFileListData(TableView<FsbFileListDataEntry>),
-    CheckObjectScriptList(TableView<CheckObjectScriptList>),
-    HitThunderboltScriptList(TableView<HitThunderboltScriptList>),
-    ReplaceScriptList(TableView<ReplaceScriptList>),
-    TimeOutScriptList(TableView<TimeOutScriptList>),
-    TouchAreaScriptList(TableView<TouchAreaScriptList>),
-    Unknown,
-}
-#[derive(Debug, Clone)]
 pub struct Table {
     name: String,
     kind: TableKind,
 }
 
-impl Table {
-    pub fn resolve<R, E>(
-        name: &str,
-        data: &[u8],
-        offset: usize,
-        resolve_string: &mut R,
-        is_relocated: &mut E,
-    ) -> Result<Self>
-    where
-        R: FnMut(u32) -> Result<StringId>,
-        E: FnMut(u32) -> bool,
-    {
-        let kind = if name == BackFromAttractionScriptList::type_name() {
-            TableKind::BackFromAttractionScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == FsbFileListDataEntry::type_name() {
-            TableKind::FsbFileListData(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == EnterZoneScriptList::type_name() {
-            TableKind::EnterZoneScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == CheckObjectScriptList::type_name() {
-            TableKind::CheckObjectScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == HitThunderboltScriptList::type_name() {
-            TableKind::HitThunderboltScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == ReplaceScriptList::type_name(){
-            TableKind::ReplaceScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == TimeOutScriptList::type_name(){
-            TableKind::TimeOutScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else if name == TouchAreaScriptList::type_name(){
-            TableKind::TouchAreaScriptList(TableView::discover(
-                data,
-                offset,
-                resolve_string,
-                is_relocated,
-            )?)
-        } else {
-            TableKind::Unknown
-        };
-
-        Ok(Self {
-            name: name.to_owned(),
-            kind,
-        })
-    }
+macro_rules! table_types {
+    ($m:ident) => {
+        $m! {
+            BackFromAttractionScriptList => BackFromAttractionScriptList,
+            EnterZoneScriptList           => EnterZoneScriptList,
+            FsbFileListDataEntry          => FsbFileListData,
+            CheckObjectScriptList         => CheckObjectScriptList,
+            HitThunderboltScriptList      => HitThunderboltScriptList,
+            ReplaceScriptList             => ReplaceScriptList,
+            TimeOutScriptList             => TimeOutScriptList,
+            TouchAreaScriptList           => TouchAreaScriptList,
+        }
+    };
 }
+macro_rules! make_table_kind {
+    (
+        $(
+            $entry:ident => $variant:ident,
+        )*
+    ) => {
+        #[derive(Debug, Clone)]
+        pub enum TableKind {
+            $(
+                $variant(TableView<$entry>),
+            )*
+            Unknown,
+        }
+    };
+}
+
+table_types!(make_table_kind);
+
+macro_rules! make_resolve {
+    (
+        $(
+            $entry:ident => $variant:ident,
+        )*
+    ) => {
+        impl Table {
+            pub fn resolve<R, E>(
+                name: &str,
+                data: &[u8],
+                offset: usize,
+                resolve_string: &mut R,
+                is_relocated: &mut E,
+            ) -> Result<Self>
+            where
+                R: FnMut(u32) -> Result<StringId>,
+                E: FnMut(u32) -> bool,
+            {
+                $(
+                    if name == $entry::type_name() {
+                        return Ok(Self {
+                            name: name.to_owned(),
+                            kind: TableKind::$variant(
+                                TableView::discover(
+                                    data,
+                                    offset,
+                                    resolve_string,
+                                    is_relocated,
+                                )?,
+                            ),
+                        });
+                    }
+                )*
+
+                Ok(Self {
+                    name: name.to_owned(),
+                    kind: TableKind::Unknown,
+                })
+            }
+        }
+    };
+}
+
+table_types!(make_resolve);
